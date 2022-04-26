4a3922846cc0cf7e7f5b14695b526cd410212227
KrevetkaOo <romantik2006@gmail.com>
2022-04-26 03:43:01 +0200

    master

--- script.js
+++ script.js
@@ -167,6 +167,75 @@ function createElement(element, attribute, inner) {
 	return el
 }
 
+function text(node, textArray) {
+	class TextScramble {
+		constructor(el) {
+			this.el = el
+			this.chars = '!<>-_\\/[]{}—=+*^?#_'
+			this.update = this.update.bind(this)
+		}
+		setText(newText) {
+			const oldText = this.el.innerText
+			const length = Math.max(oldText.length, newText.length)
+			const promise = new Promise(resolve => (this.resolve = resolve))
+			this.queue = []
+			for (let i = 0; i < length; i++) {
+				const from = oldText[i] || ''
+				const to = newText[i] || ''
+				const start = Math.floor(Math.random() * 100)
+				const end = start + Math.floor(Math.random() * 100)
+				this.queue.push({ from, to, start, end })
+			}
+			cancelAnimationFrame(this.frameRequest)
+			this.frame = 0
+			this.update()
+			return promise
+		}
+		update() {
+			let output = ''
+			let complete = 0
+			for (let i = 0, n = this.queue.length; i < n; i++) {
+				let { from, to, start, end, char } = this.queue[i]
+				if (this.frame >= end) {
+					complete++
+					output += to
+				} else if (this.frame >= start) {
+					if (!char || Math.random() < 0.28) {
+						char = this.randomChar()
+						this.queue[i].char = char
+					}
+					output += char
+				} else {
+					output += from
+				}
+			}
+			this.el.innerHTML = output
+			if (complete === this.queue.length) {
+				this.resolve()
+			} else {
+				this.frameRequest = requestAnimationFrame(this.update)
+				this.frame++
+			}
+		}
+		randomChar() {
+			return this.chars[Math.floor(Math.random() * this.chars.length)]
+		}
+	}
+	const phrases = textArray
+	const fx = new TextScramble(node)
+
+	let counter = 0
+	const next = () => {
+		fx.setText(phrases[counter]).then(() => {
+			setTimeout(next, 5000)
+		})
+		counter = (counter + 1) % phrases.length
+	}
+
+	next()
+}
+text(document.querySelector('.text-kadabra'), ['Быстрый левелинг по актам', 'Изи проход'])
+
 document.addEventListener('DOMContentLoaded', function (e) {
 	const menu = document.getElementById('act_menu')
 	const act_img = document.getElementById('act_img')
